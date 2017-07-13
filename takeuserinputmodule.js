cfc91552ca41c35ddc1cdcf7d6b6180535ba296c
EddHydraDesign <edwardthomasprice@gmail.com>
2017-07-13 16:28:02 +0100

    light changes

---
+++ takeuserinputmodule.js
@@ -0,0 +1,35 @@
+exports.myDateTime = function () {
+    return Date();
+};
+
+
+
+
+
+
+function takeuserinput(userInput){
+
+
+
+    console.log("you have written:", userInput);
+
+
+}
+
+
+
+//simulate a 5 secxond opertaion
+
+function displaytexttouser(waittime){
+
+
+    setTimeout(waittime, 5000);
+}
+
+//simulate user input
+
+userInput("test");
+userInput("test2");
+userInput("test3");
+userInput("test4");
+userInput("test5");
